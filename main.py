cd921681ce559f0de5fb19254fca776852902b74
Augusto Evangelista Moreira <augusto.moreira@mercadolivre.com>
2021-10-17 16:53:58 -0300

    v1.0

--- main.py
+++ main.py
@@ -18,17 +18,17 @@ def get_policy_actions(policy):
 
 def search_arr(arr, term):
     for a in arr:
-        aux_dict = {k.lower():v for k, v in d.items()}
+        aux_dict = {k.lower():v for k, v in a.items()}
         aux_term = term.lower()
         if aux_term in aux_dict:
             return aux_dict.get(aux_term)
 
 
-def get_aws_services_actions(service_name, action_name):
+def get_aws_service_actions(service_name, action_name):
     actions = get_json_file('services/actions.json')
     action_url = f'https://console.aws.amazon.com/iam/api/services/{service_name}/actions/{action_name}'
-    x = search_arr(actions, action_url)
-    print(x)
+    service = search_arr(actions, action_url)
+    return service
 
 
 def main():
@@ -38,11 +38,14 @@ def main():
     policy = get_json_file(args.path)
     policy_actions = get_policy_actions(policy)
 
+    print('************* Searching write actions on policy *************')
     for pa in policy_actions:
         pac = pa.split(':')
         service_name = pac[0]
         action_name = pac[1]
-        get_aws_services_actions(service_name, action_name)
+        service = get_aws_service_actions(service_name, action_name)
+        if service['actionGroups'][0] == 'ReadWrite':
+            print(f'write permission found in service: {service_name}, action {action_name}')
 
 
 # Press the green button in the gutter to run the script.
